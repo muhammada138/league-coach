a3f49e9b01a10f844cde3716a70b874666e9ee44
Muhammad Abdullah <128553002+muhammada138@users.noreply.github.com>
2026-04-11 17:33:59 -0600

    perf: riot cache

--- backend/app/state.py
+++ backend/app/state.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,9 +16,35 @@ RIOT_HEADERS = {"X-Riot-Token": RIOT_API_KEY}
 # Global semaphore to limit concurrent requests to the Riot API (prevents 429 Too Many Requests)
 api_semaphore = asyncio.Semaphore(15)
 
-# Simple in-memory TTL cache
-_cache: dict = {}
-_CACHE_TTL = 300
+class TTLCache:
+    def __init__(self, ttl_seconds: int):
+        self.ttl = ttl_seconds
+        self.cache = {}
 
-# Global cache for rank lookups
-rank_cache = {}
+    def get(self, key):
+        if key in self.cache:
+            val, expiry = self.cache[key]
+            if time.time() < expiry:
+                return val
+            else:
+                del self.cache[key]
+        return None
+
+    def set(self, key, value):
+        self.cache[key] = (value, time.time() + self.ttl)
+
+    def __contains__(self, key):
+        return self.get(key) is not None
+
+    def __getitem__(self, key):
+        val = self.get(key)
+        if val is None:
+            raise KeyError(key)
+        return val
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+# Global caches for rank lookups and match timelines
+rank_cache = TTLCache(ttl_seconds=3600)  # 1 hour
+timeline_cache = TTLCache(ttl_seconds=86400)  # 24 hours
